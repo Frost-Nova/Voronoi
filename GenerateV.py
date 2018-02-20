f4e3c0be9bd4ca91a9e6a485586c79a06d8d1f18
Harry <35147752+lhyfirefly@users.noreply.github.com>
2018-02-20 15:18:59 -0500

    Add files via upload

--- GenerateV.py
+++ GenerateV.py
@@ -1,67 +1,51 @@
+'''
+Created on Nov 8, 2017
+
+@author: lihy
+'''
 from PIL import Image
 import random
 import math
-import time
 
-class Node:
-    def __init__(self):
-        self.nx=0
-        self.ny=0
-        self.nr=0
-        self.ng=0
-        self.nb=0
-        self.near=[]
-        
-def generateVoronoiArt(img,num_cells):
-    w=img.width
-    h=img.height
-    N=num_cells
-    lamda=4*math.sqrt(w*h/N)
-    nodeList=[Node() for i in range(N)]
+im1 = Image.open("sunset.jpg")
+
+def generate_voronoi_diagram(img,num_cells):
+    im=img
+    w=im.width
+    h=im.height
+    r=[[0 for i in range(w)]for j in range(h)]
+    g=[[0 for i in range(w)]for j in range(h)]
+    b=[[0 for i in range(w)]for j in range(h)]
     
     image = Image.new("RGB", (w, h))
     putpixel = image.putpixel
-    for i in range(N):
-        nodeList[i].nx=random.randrange(w)
-        nodeList[i].ny=random.randrange(h)
-        (nodeList[i].nr,nodeList[i].ng,nodeList[i].nb)=img.getpixel((nodeList[i].nx,nodeList[i].ny))
-    
-    for i in range(N):
-        for j in range(N):
-            d = math.hypot(nodeList[i].nx-nodeList[j].nx, nodeList[i].ny-nodeList[j].ny)
-            if d<lamda:
-                nodeList[i].near.append(nodeList[j])
-
-    for y in range(h):
-        dmin = math.hypot(w-1, h-1)
-        j = -1
-        for i in range(N):
-            d = math.hypot(nodeList[i].nx, nodeList[i].ny-y)
-            if d < dmin:
-                dmin = d
-                j = i
-        for x in range(w):
-            dmin=w
-            for n in nodeList[j].near:
-                d = math.hypot(n.nx-x, n.ny-y)
+    imgx, imgy = image.size
+    nx = []
+    ny = []
+    nr = []
+    ng = []
+    nb = []
+    for i in range(num_cells):
+        nx.append(random.randrange(imgx))
+        ny.append(random.randrange(imgy))
+        (r[ny[i]][nx[i]],g[ny[i]][nx[i]],b[ny[i]][nx[i]])=im.getpixel((nx[i],ny[i]))
+        nr.append(r[ny[i]][nx[i]])
+        ng.append(g[ny[i]][nx[i]])
+        nb.append(b[ny[i]][nx[i]])
+    for y in range(imgy):
+        for x in range(imgx):
+            dmin = math.hypot(imgx-1, imgy-1)
+            j = -1
+            for i in range(num_cells):
+                d = math.hypot(nx[i]-x, ny[i]-y)
                 if d < dmin:
                     dmin = d
-                    j=nodeList.index(n)
-            putpixel((x, y), (nodeList[j].nr, nodeList[j].ng, nodeList[j].nb))
-        
-    image.save("UVA20000.png", "PNG")
+                    j = i
+            putpixel((x, y), (nr[j], ng[j], nb[j]))
+    image.save("sunset50000.png", "PNG")
     image.show()
-    
-    for n in nodeList[0].near:
-        print(str(n.nx)+' '+str(n.ny))
-    print('current node position:'+str(nodeList[0].nx)+','+str(nodeList[0].ny))    
-    print('number of nearest node is:'+str(len(nodeList[0].near)))
 
-start = time.time()
-im1 = Image.open("UVA.jpg")
-generateVoronoiArt(im1,20000)
+generate_voronoi_diagram(im1, 50000)
+
+print('Finished!')
 im1.close()
-end = time.time()
-
-print('Running time is:'+str(end-start))
-
